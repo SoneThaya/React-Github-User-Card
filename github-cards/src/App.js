138ab6cfff692dc137eee26c59af6fe02d83e5cb
Sone <sthaya@gmail.com>
2020-05-05 17:29:17 -0700

    installed material ui, styled card for rendered followers.

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -47,7 +47,7 @@ export default class App extends React.Component {
     <div className="App">
         <h1>Github Users</h1>
       <User userData={this.state.userData} />
-      <Followers />
+      <Followers followers={this.state.followers} />
     </div>
   );
 }
--- github-cards/src/components/Followers.js
+++ github-cards/src/components/Followers.js
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import IconButton from '@material-ui/core/IconButton';
 
-export default function Followers() {
+import { red } from '@material-ui/core/colors';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
+
+
+export default function Followers({ followers }) {
+
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>hellow from followers</h1>
-    </div>
+    <>
+      {followers.map(follower => (
+        <Card className={classes.root} key={follower.id}>
+    <CardHeader
+      
+      action={
+        <IconButton aria-label="settings">
+        
+        </IconButton>
+      }
+      title={follower.login}
+      
+    />
+    <img
+     
+      src={follower.avatar_url}
+         
+          alt='profile'
+    />
+    
+   
+  
+  </Card>
+      ))
+
+      }
+    
+);
+
+    </>
   )
 }
